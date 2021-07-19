73d0367167c25bdea85846524bcdbbc7eef15b29
freemjstudio <mjwoo0@naver.com>
2021-07-19 09:35:41 +0900

    1110 :smiley_cat:

--- venv/1110.py
+++ venv/1110.py
@@ -1,2 +1,19 @@
 # 1110 더하기 사이클
 
+n = int(input()) # 주어지는 숫자
+num = n
+count = 0
+result = 0
+temp = 0
+
+while True:
+    temp = n//10 + n%10
+    result = (n%10)*10 + temp%10
+    count += 1
+    n = result # 여기가 포인트네 ! 
+    if result == num:
+        break
+
+print(count)
+
+
