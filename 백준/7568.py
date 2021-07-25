87c9cbe26b72e421f1a6e6450d857b3b545b12a0
freemjstudio <mjwoo0@naver.com>
2021-07-25 11:26:28 +0900

    7568 brute force

--- 백준/7568.py
+++ 백준/7568.py
@@ -1,2 +1,15 @@
 # 7568
 n = int(input()) # 전체 사람의 수
+students = []
+
+for i in range(n):
+    x, y = map(int, input().split())
+    students.append((x,y))
+
+for i in students:
+    rank = 1
+    for j in students:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end= " ")
+
