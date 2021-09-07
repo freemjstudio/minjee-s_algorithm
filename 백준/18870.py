436da119e056387908f4a1f263203e69be71cf9c
freemjstudio <mjwoo0@naver.com>
2021-09-07 15:42:08 +0900

    Z python

--- 백준/18870.py
+++ 백준/18870.py
@@ -1,1 +1,10 @@
-# 18870 
+# 18870
+import sys
+input = sys.stdin.readline
+N = int(input())
+points = list(map(int, input().split()))
+sorted_points = sorted(list(set(points)))
+dic = {sorted_points[i] : i for i in range(len(sorted_points))}
+for point in points:
+    print(dic[point], end=' ')
+
