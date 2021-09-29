600835758d48f9f4ac0a724fbb7431cd358effd6
freemjstudio <mjwoo0@naver.com>
2021-09-30 07:30:52 +0900

    정렬

---
+++ 알고리즘책/ch06.py
@@ -0,0 +1,15 @@
+# 선택 정렬 소스코드
+
+array = [5,2,3,4,1,6,8,7,9]
+
+
+for i in range(len(array)):
+    min_index = i # 가장 작은 원소의 인덱스
+    for j in range(i+1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
+
+print(array)
+
+
