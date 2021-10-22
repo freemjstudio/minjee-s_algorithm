78acab3e41ff8993a7e33942fb005c54a07f3c0b
freemjstudio <mjwoo0@naver.com>
2021-10-22 09:22:57 +0900

    :polar_bear: :bear:

--- 알고리즘책/구현-01.py
+++ 알고리즘책/구현-01.py
@@ -14,3 +14,20 @@ if temp1 == temp2:
 else:
     print('READY')
 
+# 다른 풀이
+
+n = input()
+length = len(n)
+summary = 0
+
+# 왼쪽 부분의 자릿수 합 더하기
+for i in range(length //2):
+    summary += int(n[i])
+
+for i in range(length//2, length):
+    summary -= int(n[i])
+
+if summary == 0:
+    print("LUCKY")
+else:
+    print("READY")
