bc1da5767377b719d4514a9178f8f730cd966d6c
freemjstudio <mjwoo0@naver.com>
2021-10-26 11:43:55 +0900

    구현

--- 백준/3190-1.py
+++ 백준/3190-1.py
@@ -3,4 +3,3 @@ k = int(input())
 data = [[0]*(n+1) for _ in range(n+1)]
 info = []
 
-for _ in range
--- 알고리즘책/pg110.py
+++ 알고리즘책/pg110.py
@@ -1,22 +1,21 @@
-N = int(input())
-x,y = 1,1
-plans = input().split()
-# L R U D
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
-move_types = ['L','R','U','D']
+n = int(input())
+data = list(input().split())
 
-# 이동 계획을 하나씩 확인
-for plan in plans:
-    for i in range(len(move_types)):
-        if plan == move_types[i]:
+# L R U D
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+
+x = 1
+y = 1 # 현재 위치
+for plan in data:
+    for i in len(move_types):
+        if move_types[i] == plan:
             nx = x + dx[i]
             ny = y + dy[i]
-        # 공간을 벗어나는 경우는 무시한다
-        if nx < 1 or ny < 1 or nx > N or ny > N:
-            continue
-        x,y = nx,ny # 이동하기
+    # 공간을 넘어가면 무시해야 한다
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny
 
-print(x,y)
-
-
+print(x, y)
