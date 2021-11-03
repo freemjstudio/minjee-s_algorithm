0ba02afa821b4a450f3e5aa6c25b11f599b3392b
freemjstudio <mjwoo0@naver.com>
2021-11-03 22:47:01 +0900

    bfs

--- 백준/18405.py
+++ 백준/18405.py
@@ -1,36 +1,38 @@
 # 경쟁적 전염
-import sys
-sys.setrecursionlimit(100000)
-n, k = map(int, sys.stdin.readline().split())
+from collections import deque
+
+n, k = map(int, input().split())
+
+graph = []
 data = []
-for _ in range(n):
-    data.append(list(map(int, sys.stdin.readline().split())))
+for i in range(n):
+    graph.append(list(map(int, input().split())))
 
-s, x, y = map(int, sys.stdin.readline().split())
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] != 0:
+            data.append((graph[i][j],0,i,j)) # 바이러스 종류 - s - x - y
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+s, target_x, target_y = map(int, input().split())
 
-def virus(ax,ay,v): # virus 종류는 v
-    for i in range(4):
-        nx = ax + dx[i]
-        ny = ay + dy[i]
-        if nx >= 0 and ny >= 0 and nx < n and ny < n:
-            if data[nx][ny] == 0:
-                data[nx][ny] = v
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
 
+data.sort()
+q = deque(data)
 
-while True:
-    count = 1
-    for v in range(k+1):
-        for i in range(n):
-            for j in range(n):
-                if data[i][j] == v:
-                    virus(i,j,v)
-    count += 1
-    if count == s:
+while q:
+    virus, time, x, y = q.popleft()
+    if time == s:
         break
 
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
+        if nx >= 0 and nx < n and ny >= 0 and ny < n:
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = virus
+                q.append((virus, time+1, nx, ny))
 
-print(data[x-1][y-1])
+print(graph[target_x-1][target_y-1])
