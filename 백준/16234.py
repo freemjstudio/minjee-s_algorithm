7e6b188a2f13e3801138070b4b991a33fec4cb25
freemjstudio <mjwoo0@naver.com>
2021-11-07 10:02:08 +0900

    bfs

--- 백준/16234.py
+++ 백준/16234.py
@@ -21,5 +21,43 @@ def process(x, y, index):
     # bfs를 위한 큐
     q = deque()
     q.append((x,y))
-    united[x][y] = index # 현재 연합의 번호 할당
+    union[x][y] = index # 현재 연합의 번호
     summary = graph[x][y]  # 현재 연합의 전체 인구 수
+    count = 1 # 현재 연합의 국가 수
+    # 큐가 빌 때까지 반복한다
+    while q:
+        x, y = q.popleft()
+        # 현재 위치에서 4가지 방향을 확인한다.
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 바로 옆에 있는 나라를 확인하며
+            if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
+                # 옆에 있는 나라와 인구 차이가 L명 이상 R명 이하라면
+                if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
+                    q.append((nx, ny))
+                    # 연합에 추가하기
+                    union[nx][ny] = index
+                    summary += graph[nx][ny]
+                    count += 1
+                    united.append((nx, ny))
+    for i, j in united:
+        graph[i][j] == summary // count
+    return count
+
+total_count = 0
+
+# 더 이상 인구 이동을 할 수 없을 떄까지 반복한다.
+while True:
+    union = [[-1] * n for _ in range(n)]
+    index = 0
+    for i in range(n):
+        for j in range(n):
+            if union[i][j] == -1: # 해당 나라가 아직 처리 되지 않았다면
+                process(i, j, index)
+                index += 1
+    if index == n*n: # 인구 이동이 끝난 경우
+        break
+    total_count += 1
+# 인구 이동 횟수 출력
+print(total_count)
