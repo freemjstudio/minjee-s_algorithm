e6a6c76f17fb99c2d8608555999a3a020f824757
freemjstudio <mjwoo0@naver.com>
2021-11-07 12:50:50 +0900

    1715

--- 백준/1715.py
+++ 백준/1715.py
@@ -3,23 +3,16 @@ import heapq
 
 n = int(input())
 heap = []
-
 for i in range(n):
     data = int(input())
-    heapq.heappush(heap, data)
+    heapq.heappush(heap,data)
 
 result = 0
 
-# heap에 원소가 1개 남을 때까지
-
 while len(heap) != 1:
-    # 가장 작은 2개의 카드 묶음 꺼내기
     one = heapq.heappop(heap)
-    two = heapq.heappop(heap)
-
-    # 카드 묶음을 합쳐서 다시 삽입
-    sum_value = one + two
-    result += sum_value
-    heapq.heappush(heap, sum_value)
-
+    two = heapq.heappop(heap) # 카드 더미 두개씩 더해 나가야 하니까
+    temp_sum = one + two
+    result += temp_sum
+    heapq.heappush(heap, temp_sum)
 print(result)
