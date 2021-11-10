6791e1351329c29e76b7818d15906baa9b208c5e
freemjstudio <mjwoo0@naver.com>
2021-11-11 08:27:05 +0900

    binary search

--- 백준/pg367.py
+++ 백준/pg367.py
@@ -1,20 +1,39 @@
 n, x = map(int, input().split())
-numbers = list(map(int, input().split()))
-count = 0
+array = list(map(int, input().split()))
 
-def binary(array, target, start, end):
-    global count;
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == target:
-            count += 1
-            binary(array, target, start, mid-1)
-            binary(array, target, mid + 1, end)
-        elif array[mid] > target:
-            end = mid -1
-        else:
-            start = mid + 1
+def count_by_value(array, x):
+    a = first(array, x, 0, n -1)
+    if a == None:
+        return 0
+    b = last(array, x, 0, n-1)
+    return b - a + 1
+
+def first(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) //2
+    if (mid == 0 or target > array[mid -1]) and array[mid] == target:
+        return mid
+    elif target <= array[mid]:
+        return first(array, target, start, mid -1)
+    else:
+        return first(array, target, mid + 1, end)
+
+def last(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) //2
+    if (mid == n-1 or target < array[mid +1]) and array[mid] == target:
+        return mid
+    elif target <= array[mid]:
+        return first(array, target, start, mid -1)
+    else:
+        return first(array, target, mid+1, end)
 
 
-binary(numbers, x, 0, n-1)
+count = count_by_value(array, x)
 
+if count == 0:
+    print(-1)
+else:
+    print(count)
