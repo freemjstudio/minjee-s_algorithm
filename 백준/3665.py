32d63605dd2c98e490ad0e2ce53cb799ea061971
freemjstudio <mjwoo0@naver.com>
2021-12-09 23:32:20 +0900

    3665 BOJ

--- 백준/3665.py
+++ 백준/3665.py
@@ -31,3 +31,39 @@ for _ in range(t):
             graph[b][a] = False # b <- a
             indegree[a] -= 1
             indegree[b] += 1
+
+    # 위상 정렬 알고리즘
+    result = []
+    q = deque()
+    for i in range(1, n+1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    certain = True # 위상 정렬 결과가 하나인가 ?
+    cycle = False # 그래프 내 사이클이 존재하는가
+
+    for i in range(n): # 노드 개수 만큼 반복
+        if len(q) == 0:
+            cycle = True
+            break
+        if len(q) >= 2: # 한번에 진입차수가 동일한 두개이상의 노드가 동시에 들어오면 결과가 여러개가 된다
+            certain = False
+            break
+        now = q.popleft()
+        result.append(now)
+        # 진입차수 갱신
+        for j in range(1, n+1):
+            if graph[now][j]:
+                indegree[j] -= 1
+                if indegree[j] == 0:
+                    q.append(j)
+
+    # 결과 출력
+    if cycle:
+        print("IMPOSSIBLE")
+    elif not certain:
+        print("?")
+    else:
+        for i in result:
+            print(i, end=' ')
+        print()
