bef1c1be16281527aebadc92e765d548f0e21d2b
freemjstudio <mjwoo0@naver.com>
2021-12-26 12:17:31 +0900

    11053

--- 백준/11053.py
+++ 백준/11053.py
@@ -2,3 +2,13 @@
 
 n = int(input())
 array = list(map(int, input().split()))
+
+dp = [0]*n
+for i in range(n):
+    for j in range(i):
+        if array[j] < array[i] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+
+
+print(max(dp))
