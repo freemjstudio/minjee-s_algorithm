0a4541503b3370f7f9863e533331c9a4f281ab45
freemjstudio <mjwoo0@naver.com>
2022-01-05 22:28:22 +0900

    구현 113p

---
+++ 알고리즘책/구현/113p.py
@@ -0,0 +1,9 @@
+n = int(input())
+result = 0
+for i in range(n+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                result += 1
+
+print(result)
