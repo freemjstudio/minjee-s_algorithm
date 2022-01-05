8c70248560dff88846a4629bbae6189c6b966b1f
freemjstudio <mjwoo0@naver.com>
2022-01-05 21:43:27 +0900

    구현 117p

--- 알고리즘책/구현/p115.py
+++ 알고리즘책/구현/p115.py
@@ -1,1 +1,16 @@
 # 115p 왕실의 나이트
+data = input()
+x = int(data[1])
+y = int(ord(data[0])) - int(ord('a')) + 1
+
+types = [(-2,-1),(-2,1), (1,-2),(-1,-2), (2,1), (2,-1), (-1,2), (1,2)]
+
+result = 0
+
+for type in types:
+    nx = x + type[0]
+    ny = y + type[1]
+    if nx >= 1 and ny >= 1 and nx <=8 and ny <=8:
+        result += 1
+
+print(result)
