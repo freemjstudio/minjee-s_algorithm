67b8b2beaff13baa7a438ed0081c84d5ecb97810
freemjstudio <mjwoo0@naver.com>
2022-01-28 19:31:45 +0900

    Spanning Tree

--- 알고리즘책/Graph/395.py
+++ 알고리즘책/Graph/395.py
@@ -1,16 +1,17 @@
-g = int(input())
-p = int(input())
+g = int(input()) # 탑승구 1 ~ G번까지의 번호로 구분
+p = int(input()) # 비행기 개수
 
-parent = [0] * (g+1)
-for i in range(1, g+1):
+parent = [0] * (g+1) # 탑승구 node 들
+
+for i in range(1, g+1): # 현재 node 들 각각은 자기 자신을 가리키고 있다.
     parent[i] = i
 
 def find_parent(parent, x):
     if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
+        parent[x] = find_parent(parent, x)
     return parent[x]
 
-def union_parent(parent, a, b):
+def union(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
     if a < b:
@@ -19,11 +20,12 @@ def union_parent(parent, a, b):
         parent[a] = b
 
 result = 0
-for i in range(p):
-    data = int(input())
-    if find_parent(parent,data) != 0:
-        union_parent(parent, data, data-1)
-        result += 1
-    else:
+
+for _ in range(p):
+    data = find_parent(parent, int(input()))
+    if data == 0:
         break
+    union(parent, data, data-1) # 바로 왼쪽의 집합과 합쳐준다.
+    result += 1
+
 print(result)
--- 알고리즘책/Graph/397.py
+++ 알고리즘책/Graph/397.py
@@ -2,17 +2,14 @@
 n, m = map(int, input().split())
 edges = []
 for i in range(m):
-    a, b, cost = map(int, input().split())
-    edges.append((cost, a, b))
+    x, y, cost = map(int, input().split())
+    edges.append((cost, x, y)) # 비용 순으로 정렬하기 위해서 cost부터 써준다.
 
-edges.sort()
-parent = [0] * (n+1)
-for i in range(1, n+1):
-    parent[i] = i
+edges.sort() # 비용 순으로 정렬한다.
 
 def find_parent(parent, x):
     if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
+        parent[x] = find_parent(parent, x)
     return parent[x]
 
 def union_find(parent, a, b):
@@ -25,10 +22,14 @@ def union_find(parent, a, b):
 
 result = 0
 total = 0
+parent = [0] * (n+1)
+for i in range(1, n+1):
+    parent[i] = i
+
 for edge in edges:
     cost, a, b = edge
     total += cost
-    if find_parent(parent, a) != find_parent(parent, b):
+    if find_parent(parent, a) != find_parent(parent, b): # 사이클이 발생하지 않는 경우에만 간선에 포함한다. 그러면 최소비용으로 만들 수 있다.
         union_find(parent, a, b)
         result += cost
 
