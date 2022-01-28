ac6688eed3dd4ea1c03cf9f5c97771850889cd3d
freemjstudio <mjwoo0@naver.com>
2022-01-28 21:45:49 +0900

    3665 위상정렬

--- 백준/Graph/3665.py
+++ 백준/Graph/3665.py
@@ -1,58 +1,64 @@
 from collections import deque
 
-for _ in range(int(input())):
+for _ in range(int(input())): # test case 만큼 반복하기
     n = int(input())
-    indegree = [0] * (n+1)
-    graph = [[False] * (n+1) for _ in range(n+1)]
-    data = list(map(int, input().split())) # 작년 순위 정보 입력
+    data = list(map(int, input().split()))
+    m = int(input())
+    graph = [[False]*(n+1) for _ in range(n+1)]
+    indegree = [0] * (n + 1)  # 진입 차수
     for i in range(n):
         for j in range(i+1, n):
-            graph[data[i]][data[j]] = True # 방향 그래프 i -> j
-            indegree[data[j]] += 1 # j 진입차수 1 증가
-    m = int(input())
+            graph[data[i]][data[j]] = True # i -> j
+            indegree[data[j]] += 1
+
     for i in range(m):
-        a, b = map(int, input().split())
-        # 간선의 방향 뒤집기
+        a, b = map(int, input().split()) # 그래프 방향 바꿔주기
         if graph[a][b]:
             graph[a][b] = False
-            graph[b][a] = True
+            graph[b][a] = True # b -> a
             indegree[a] += 1
             indegree[b] -= 1
+
         else:
             graph[a][b] = True
             graph[b][a] = False
             indegree[a] -= 1
             indegree[b] += 1
-    # 위상 정렬
-    result = []
-    q = deque()
 
-    for i in range(1, n+1):
+    q = deque()
+    result = []
+    certain = True  # 결과가 오직 하나인가
+    cycle = False # 사이클 판별
+
+    for i in range(1, n+1): # node 수 만큼 판별을 해야 한다. -> q 가 비면, 사이클이 발생했다는 걸 확인
         if indegree[i] == 0:
             q.append(i)
 
-    certain = True # 위상정렬 수행 결과가 하나인가
-    cycle = False
     for i in range(n):
-        if len(q) == 0: # 큐가 비어있다면 사이클이 발생했다는 의미
+        if len(q) == 0: # 큐가 비어있으면 사이클이 발생했다는 의미
             cycle = True
             break
-        if len(q) >= 2: # 큐의 원소가 2개 이상이면 정렬 결과가 여러개
+        if len(q) >= 2:
             certain = False
             break
-
         now = q.popleft()
         result.append(now)
-        for j in range(1, n+1):
-            if graph[now][j]:
+        for j in range(1, n+1): # 해당 원소와 연결된 노드들의 진입차수에서 -1
+            if graph[now][j]: # now -> j 가 true 이면 반환
                 indegree[j] -= 1
                 if indegree[j] == 0:
                     q.append(j)
+
     if cycle:
         print("IMPOSSIBLE")
     elif not certain:
-        print("?")
+        print('?')
     else:
         for i in result:
             print(i, end=' ')
-        print()
+
+
+
+
+
+
--- 알고리즘책/Graph/398p.py
+++ 알고리즘책/Graph/398p.py
@@ -1,1 +1,54 @@
 # 행성 터널
+import sys
+sys.setrecursionlimit(10**6)
+n = int(input()) # 행성의 개수
+x = []
+y = []
+z = []
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, x)
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+parent = [0]*(n+1)
+
+for i in range(1, n+1):
+    parent[i] = i
+
+for i in range(1, n+1):
+    data = list(map(int, input().split()))
+    x.append((data[0], i)) # cost, 행성번호
+    y.append((data[1], i))
+    z.append((data[2], i))
+
+x.sort()
+y.sort()
+z.sort()
+
+edges = []
+
+for i in range(n-1):
+    edges.append((x[i+1][0] - x[i][0], x[i][1], x[i+1][1])) # cost, A node , B node
+    edges.append((y[i+1][0] - y[i][0], y[i][1], y[i+1][1]))
+    edges.append((z[i+1][0] - z[i][0], z[i][1], z[i+1][1]))
+
+
+
+edges.sort()
+result = 0
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
