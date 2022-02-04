9cf854704e8d947634701a2149b4f9b7d4f594df
freemjstudio <mjwoo0@naver.com>
2022-02-04 13:15:17 +0900

    2206 BFS

--- 백준/2206.py
+++ 백준/2206.py
@@ -2,38 +2,28 @@ from collections import deque
 
 n, m = map(int, input().split())
 graph = [list(map(int, input())) for _ in range(n)]
-INF = int(1e9)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def bfs(): # 최단 경로 구하기
-    dist = [[-1] * m for _ in range(n)]
+    dist = [[[-1]*2 for _ in range(m)] for _ in range(n)]
     q = deque()
-    q.append((0, 0))
-    dist[0][0] = 1
+    q.append((0, 0, 0)) # x, y, crash
+    dist[0][0][0] = 1 # 최단 거리
     while q:
-        x, y = q.popleft()
+        x, y, crash = q.popleft()
+        if x == n-1 and y == m-1:
+            return dist[x][y][crash]
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and dist[nx][ny] == -1 and graph[nx][ny] == 0:
-                dist[nx][ny] = dist[x][y] + 1
-                q.append((nx, ny))
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 0 and dist[nx][ny][crash] == -1:
+                    dist[nx][ny][crash] = dist[x][y][crash] + 1
+                    q.append((nx, ny, crash))
+                if graph[nx][ny] == 1 and crash == 0:
+                    dist[nx][ny][crash +1] = dist[x][y][crash] + 1
+                    q.append((nx, ny, crash+1))
+    return -1
 
-    return dist[n-1][m-1]
-
-result = INF
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            graph[i][j] = 0
-            temp = bfs()
-            if temp != -1:
-                result = min(result, temp)
-            graph[i][j] = 1
-
-
-if result == INF:
-    print(-1)
-else:
-    print(result)
+print(bfs())
