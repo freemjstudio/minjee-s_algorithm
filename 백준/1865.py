921d076de4864b9788e2af9f3803b71bfd445cf1
freemjstudio <mjwoo0@naver.com>
2022-02-09 22:45:10 +0900

    1865 웜홀

--- 백준/1865.py
+++ 백준/1865.py
@@ -1,15 +1,35 @@
 # 1865 웜홀
 INF = int(1e9)
-for _ in range(int(input())):
+t = int(input())
+
+def solve(start):
+
+    distance[start] = 0
+
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            for end, cost in graph[j]:
+                if distance[end] > distance[j] + cost:
+                    distance[end] = distance[j] + cost
+                    if i == n:
+                        return False
+    return True
+
+for _ in range(t):
     n, m, w = map(int, input().split())
-    edges = []
-    distance = [INF]*(n+1)
+    graph = [[] for _ in range(n+1)]
+    distance = [INF] * (n + 1)
     for i in range(m):
         start, end, value = map(int, input().split())
-        edges.append((start, end, value))
+        graph[start].append((end, value))
+        graph[end].append((start, value))
 
     for j in range(w):
         start, end, value = map(int, input().split())
-        edges.append((start, end, (-1)*value))
+        graph[start].append((end, -value)) # start -> end 방향으로만 존재 !
 
-    
+    result = solve(1)
+    if not result:
+        print('YES')
+    else:
+        print('NO')
