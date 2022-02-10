f1b87216c807b95acfd27f61738378d4cee19f66
freemjstudio <mjwoo0@naver.com>
2022-02-10 22:27:56 +0900

    1238 -Dijkstra

--- 백준/DFS+BFS/9466.py
+++ 백준/DFS+BFS/9466.py
@@ -1,24 +1,35 @@
 # 9466 텀프로젝트
-from collections import deque
+import heapq
 INF = int(1e9)
 n, m, x = map(int, input().split()) # N M X
 graph = [[] for _ in range(n+1)]
-dist = [-1]*(n+1) # 최단거리 기록
-dist[x] = 0 # 파티 개최하는 곳
+
 
 for i in range(m):
     s, e, t = map(int, input().split())
     graph[s].append((e, t))
 
+def dijkstra(start):
+    dist = [INF] * (n+1)
+    dist[start] = 0
+    q = []
+    heapq.heappush(q, (0, start))
+    while q:
+        d, now = heapq.heappop(q)
+        if dist[now] < d:
+            continue
+        for next, cost in graph[now]:
+            if dist[next] > dist[now] + cost:
+                dist[next] = dist[now] + cost
+                heapq.heappush(q, (dist[next], next))
+    return dist
 
 
 
-
+result = 0
 for i in range(1, n+1):
-    if i == x:
-        continue
-    else:
-        bfs(i)
+    first = dijkstra(i) # to go to x
+    second = dijkstra(x) # return from x
+    result = max(result, first[x] + second[i])
 
-
-
+print(result)
