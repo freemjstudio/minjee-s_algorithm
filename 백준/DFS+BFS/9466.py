eafa043137258f4258943fd48f04bcd4d10ed534
freemjstudio <mjwoo0@naver.com>
2022-02-11 23:00:45 +0900

    10991

--- 백준/DFS+BFS/9466.py
+++ 백준/DFS+BFS/9466.py
@@ -1,35 +1,7 @@
-# 9466 텀프로젝트
-import heapq
-INF = int(1e9)
-n, m, x = map(int, input().split()) # N M X
-graph = [[] for _ in range(n+1)]
+#9466 텀 프로젝트
 
-
-for i in range(m):
-    s, e, t = map(int, input().split())
-    graph[s].append((e, t))
-
-def dijkstra(start):
-    dist = [INF] * (n+1)
-    dist[start] = 0
-    q = []
-    heapq.heappush(q, (0, start))
-    while q:
-        d, now = heapq.heappop(q)
-        if dist[now] < d:
-            continue
-        for next, cost in graph[now]:
-            if dist[next] > dist[now] + cost:
-                dist[next] = dist[now] + cost
-                heapq.heappush(q, (dist[next], next))
-    return dist
-
-
-
-result = 0
-for i in range(1, n+1):
-    first = dijkstra(i) # to go to x
-    second = dijkstra(x) # return from x
-    result = max(result, first[x] + second[i])
-
-print(result)
+t = int(input())
+for i in range(t):
+    n = int(input())
+    array = list(map(int, input().split()))
+    
