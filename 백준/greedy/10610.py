2029387a099918f32314b6dea006c10ae38f61b6
freemjstudio <mjwoo0@naver.com>
2022-02-15 00:45:10 +0900

    10610 Greedy

--- 백준/greedy/10610.py
+++ 백준/greedy/10610.py
@@ -1,4 +1,11 @@
-t = int(input())
-for i in range(t):
-    a, b = map(int, input().split(','))
-    print(a+b)
+
+n = list(input())
+total = 0
+
+for i in n:
+    total += int(i)
+if '0' not in n or total %3 != 0:
+    print(-1)
+else:
+    n.sort(reverse=True)
+    print(''.join(n))
