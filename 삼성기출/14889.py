38d9acbbbd2fd9378128adf9749b7d4809be0aae
freemjstudio <mjwoo0@naver.com>
2022-02-18 09:47:45 +0900

    14889 스타트와링크

--- 삼성기출/14889.py
+++ 삼성기출/14889.py
@@ -9,18 +9,18 @@ for i in range(n):
     array.append(list(map(int, input().split())))
 
 cases = combinations(numbers, n//2)
-startlink = []
-for case in cases:
-    check = combinations(case, 2)
-    total = 0
-    for i in check:
-        total += array[i[0]][i[1]] + array[i[1]][i[0]]
-    startlink.append(total)
-
 result = int(1e9)
-startlink.sort()
-for i in range(len(startlink)-1):
-    diff = abs(startlink[i] - startlink[i+1])
-    if diff < result:
-        result = diff
+
+for case in cases:
+    check_start = combinations(case, 2)
+    outofcase = list(set(numbers) - set(case))
+    check_link = combinations(outofcase, 2)
+    start, link = 0, 0
+    for i in check_start:
+        start += array[i[0]][i[1]] + array[i[1]][i[0]]
+    for i in check_link:
+        link += array[i[0]][i[1]] + array[i[1]][i[0]]
+    result = min(result, abs(start-link))
+
+
 print(result)
