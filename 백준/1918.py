459d4a62331c7063ce6ca7602a026a351563206b
freemjstudio <mjwoo0@naver.com>
2022-02-21 10:23:46 +0900

    1918 stack

--- 백준/1918.py
+++ 백준/1918.py
@@ -2,31 +2,29 @@
 
 data = input()
 stack = []
-op = []
-i = 0
+result = ''
+
+for now in data:
+    if now.isalpha():
+        result += now
+    else:
+        if now == '(':
+            stack.append(now)
+        elif now == '*' or now == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                result += stack.pop()
+            stack.append(now)
+        elif now == '+' or now == '-':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.append(now)
+        else: # ')'
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.pop()
 
 
-while True:
-    if i == len(data):
-        break
-    if data[i].isalpha():
-        stack.append(data[i])
-    else: # 연산자
-        if data[i] == '/' or data[i] == '*':
-            if data[i+1].isalpha():
-                stack.append(data[i+1])
-                stack.append(data[i])
-                stack.extend(op)
-                op = []
-                i += 1
-            else:
-                stack.append(data[i])
-                stack.extend(op)
-                op = []
-        else:
-            op.append(data[i])
-    i += 1
+while stack:
+    result += stack.pop()
 
-
-for i in stack:
-    print(i, end="")
+print(result)
