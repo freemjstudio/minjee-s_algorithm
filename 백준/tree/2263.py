4f4a0fec0b31218c48fb11cf1545e6cda9bac619
freemjstudio <mjwoo0@naver.com>
2022-02-21 14:09:21 +0900

    [Boj] 트리의순회

--- 백준/tree/2263.py
+++ 백준/tree/2263.py
@@ -1,21 +1,29 @@
 # 2263 트리의 순회
+import sys
+sys.setrecursionlimit(10**6)
 
 n = int(input())
-for _ in range(n):
-    inorder = list(map(int, input().split()))
-    postorder = list(map(int, input().split()))
+position = [0]*(n+1)
 
+inorder = list(map(int, input().split()))
+postorder = list(map(int, input().split()))
+
+for i in range(n):
+    position[inorder[i]] = i
 # preorder : root -> left child -> right child
 
 def preorder(in_start, in_end, post_start, post_end):
-    if in_start > in_end or post_start > post_end:
+    # 재귀 종료 조건 - start와 end 가 만나는 지점에서 종료
+    if (in_start > in_end) or (post_start > post_end):
         return
     root = postorder[post_end]
     print(root, end=" ") # root
-    root_index = inorder.index(root)
+
+    left = position[root] - in_start
+    right = in_end - position[root]
     # left child
-    preorder(in_start, root_index-1, in_start, root_index)
+    preorder(in_start, in_start+left-1, post_start, post_start+left-1)
     # right child
-    preorder(root_index+1, in_end, root_index+1, post_end)
+    preorder(in_end-right+1, in_end, post_end - right, post_end-1)
 
 preorder(0, n-1, 0, n-1)
