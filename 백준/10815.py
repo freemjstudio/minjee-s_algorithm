7ebbc6d0f832cb7f2d759c6bdbd8e7dea656e071
freemjstudio <mjwoo0@naver.com>
2022-02-25 09:27:28 +0900

    [Boj] 10815

--- 백준/10815.py
+++ 백준/10815.py
@@ -1,1 +1,12 @@
 # 10815 숫자카드
+
+n = int(input())
+card = list(map(int, input().split()))
+m = int(input())
+find = list(map(int, input().split()))
+
+for i in find:
+    if i in card:
+        print('1', end=' ')
+    else:
+        print('0', end=' ')
