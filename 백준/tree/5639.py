c478389478b125c1f459faee35a0deea5716f70a
freemjstudio <mjwoo0@naver.com>
2022-02-25 10:44:10 +0900

    [Boj] 5639

---
+++ 백준/tree/5639.py
@@ -0,0 +1,27 @@
+# 5639 이진 검색 트리
+import sys
+sys.setrecursionlimit(10**6)
+tree = []
+
+
+def postorder(start, end):
+    if start > end:
+        return
+    root = tree[start]
+    idx = end+1
+
+    for i in range(start+1, end+1):
+        if root < tree[i]:
+            idx = i
+            break
+    postorder(start+1, idx-1)
+    postorder(idx, end)
+    print(root)
+
+while True:
+    try:
+        tree.append(int(input()))
+    except: # EOF 입력되면 에러가 일어나는것을 이용
+        break
+
+postorder(0, len(tree)-1)
