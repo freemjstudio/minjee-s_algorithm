6096e16c4e29c5de6470009057e069696da673a6
freemjstudio <mjwoo0@naver.com>
2022-02-25 09:55:20 +0900

    [Boj] 10815

--- 백준/10815.py
+++ 백준/10815.py
@@ -4,9 +4,24 @@ n = int(input())
 card = list(map(int, input().split()))
 m = int(input())
 find = list(map(int, input().split()))
+card.sort()
+
+def bisect(left, right, target):
+
+    if left > right:
+        return None
+    mid = (left + right) // 2
+    if target == card[mid]:
+        return mid
+    elif card[mid] < target:
+        return bisect(mid+1, right, target)
+    else:
+        return bisect(left, mid - 1, target)
 
 for i in find:
-    if i in card:
-        print('1', end=' ')
+    result = bisect(0, n-1, i)
+    if result == None:
+        print('0',end=' ')
     else:
-        print('0', end=' ')
+        print('1', end=' ')
+
