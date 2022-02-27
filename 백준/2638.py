6a831090e4d6765aef2d2a3b341d02145d04186b
freemjstudio <mjwoo0@naver.com>
2022-02-28 01:57:33 +0900

    [Boj] 2638

--- 백준/2638.py
+++ 백준/2638.py
@@ -2,17 +2,44 @@
 from collections import deque
 
 n, m = map(int, input().split())
-array = []
-
-for i in range(n):
-    array.append(list(map(int, input().split())))
+array = [list(map(int, input().split())) for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-count = 0 # result
+count = 0
+while True:
+    flag = 0
 
-def bfs():
+    q = deque()
+    visited = [[0 for _ in range(m)] for _ in range(n)]
 
+    q.append((0, 0))
+    visited[0][0] = 1
 
+    while q:
+        x, y = q.popleft()
 
-print(count)
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0:
+                if array[nx][ny]:
+                    array[nx][ny] += 1
+                else:
+                    visited[nx][ny] = 1
+                    q.append((nx, ny))
+
+    for i in range(n):
+        for j in range(n):
+            if array[i][j] >= 3:
+                array[i][j] = 0
+            elif array[i][j] > 0:
+                flag = 1
+                array[i][j] = 1
+
+    count += 1
+
+    if not flag:
+        print(count)
+        break
