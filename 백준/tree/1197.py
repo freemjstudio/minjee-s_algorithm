0cc17e5477b0c209992768a208857f873c2a487c
freemjstudio <mjwoo0@naver.com>
2022-02-28 23:47:18 +0900

    [Boj] 1197

--- 백준/tree/1197.py
+++ 백준/tree/1197.py
@@ -1,4 +1,31 @@
 # 1197 최소 스패닝 트리
-
 v, e = map(int, input().split())
-for _ in range(v):
+edges = []
+parent = [0]*(v+1)
+for i in range(1, v+1):
+    parent[i] = i
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+for i in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+edges.sort() # sort by cost
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+result = 0
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+print(result)
