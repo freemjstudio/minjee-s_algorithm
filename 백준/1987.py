e754bdf3efa8ba201a1fb4850fe7cc4efe1478ef
freemjstudio <mjwoo0@naver.com>
2022-03-01 18:34:36 +0900

    [Boj] 1987 backtracking

--- 백준/1987.py
+++ 백준/1987.py
@@ -1,7 +1,4 @@
 # 1987 알파벳
-from collections import deque
-
-
 r, c = map(int, input().split())
 array = []
 for _ in range(r):
@@ -10,31 +7,22 @@ for _ in range(r):
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-max_count = 0
+count = 1
+alphabet = []
+def dfs(x, y, cnt):
 
-def bfs():
-    global max_count
-    q = deque()
-    q.append((0,0))
-    alphabet = []
-    visited = [[0]*c for _ in range(r)]
-    visited[0][0] = 1
-    alphabet.append(array[0][0])
+    global count
+    count = max(cnt, count)
 
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < r and 0 <= ny < c:
-                if visited[nx][nx] == 0:
-                    visited[nx][ny] = 1
-                    if array[nx][ny] not in alphabet:
-                        alphabet.append(array[nx][ny])
-                        if max_count < len(alphabet):
-                            max_count = len(alphabet)
-                        q.append((nx, ny))
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
+        if 0 <= nx < r and 0 <= ny < c:
+            if array[nx][ny] not in alphabet:
+                alphabet.append(array[nx][ny])
+                dfs(nx, ny, cnt+1)
+                alphabet.pop()
 
-bfs()
-print(max_count)
+dfs(0, 0, 1)
+print(count)
