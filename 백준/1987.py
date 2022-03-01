e8bbea31eaa6db50043667839ab02a23b48eaec3
freemjstudio <mjwoo0@naver.com>
2022-03-01 18:59:36 +0900

    [Boj] 1987 backtracking

--- 백준/1987.py
+++ 백준/1987.py
@@ -1,14 +1,15 @@
 # 1987 알파벳
-r, c = map(int, input().split())
-array = []
-for _ in range(r):
-    array.append(input())
+import sys
+
+r, c = map(int, sys.stdin.readline().split())
+array = [list(map(lambda x:ord(x)-65, input().rstrip())) for _ in range(r)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 count = 1
-alphabet = []
+alphabet = [0] * 26
+
 def dfs(x, y, cnt):
     global count
     count = max(cnt, count)
@@ -17,12 +18,12 @@ def dfs(x, y, cnt):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if 0 <= nx < r and 0 <= ny < c:
-            if array[nx][ny] not in alphabet:
-                alphabet.append(array[nx][ny])
+        if 0 <= nx < r and 0 <= ny < c and alphabet[array[nx][ny]] != 1:
+                alphabet[array[nx][ny]] = 1
                 dfs(nx, ny, cnt+1)
-                alphabet.remove(array[nx][ny])
+                alphabet[array[nx][ny]] = 0
 
-alphabet.append(array[0][0])
+alphabet[array[0][0]] = 1
+
 dfs(0, 0, count)
 print(count)
