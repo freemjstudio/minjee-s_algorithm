d100b0ffe36bcceb3e1c25606fd873c422c33496
freemjstudio <mjwoo0@naver.com>
2022-09-05 09:59:05 +0900

    [Leetcode] 125

--- leetcode/leetcode_125.py
+++ leetcode/leetcode_125.py
@@ -1,2 +1,18 @@
-# leetcode valid palindrome
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # 예외 처리
+        if s == " ":
+            return True
 
+        s = s.lower()  # 소문자로 변환
+        new_str = ""
+
+        for i in s:
+            if i.isalnum():
+                new_str += i
+        n = len(new_str)
+
+        for i in range(n // 2):
+            if new_str[i] != new_str[n - i - 1]:
+                return False
+        return True
