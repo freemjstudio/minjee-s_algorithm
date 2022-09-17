128b0226d7956232a5fa952807013966b1fdb6a5
freemjstudio <mjwoo0@naver.com>
2022-09-17 23:10:15 +0900

    1913

--- 백준/구현/1913.py
+++ 백준/구현/1913.py
@@ -1,39 +1,36 @@
 # 1913
+import sys
 
 n = int(input())
 find = int(input()) # 찾고자 하는 숫자
 find_x, find_y = 0, 0
-array = [[0]*n for _ in range(n)]
+array = [[0 for _ in range(n)] for _ in range(n)]
 
 # 방향 : 아래, 오른쪽, 위, 왼쪽
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
-dir = 0
+direction = 0
 now = 0
-count = n*n
+count = n**2
 x, y = 0, 0
 array[x][y] = count
 count -= 1
 
 while count > 0:
-    nx = x + dx[dir]
-    ny = y + dy[dir]
-    if 0 <= nx < n and 0 <= ny < n and array[x][y] == 0:
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+
+    if 0 <= nx < n and 0 <= ny < n and not array[nx][ny]:
         array[nx][ny] = count
         if count == find:
             find_x, find_y = nx, ny
-        count -= 1
         x, y = nx, ny
+        count -= 1
     else:
-        dir = (dir+1)%4
-
+        direction = (direction+1) % 4
 
 
 # 표 출력하기
-for i in range(n):
-    for j in range(n):
-        print(array[i][j], end=" ")
-        if array[i][j] == find:
-            find_x, find_y = i, j
-    print()
+for row in array:
+    print(*row)
 print(find_x+1, find_y+1)
