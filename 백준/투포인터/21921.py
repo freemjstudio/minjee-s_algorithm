6bbdf0024e890136a389959e75891ca7ef3a8328
freemjstudio <mjwoo0@naver.com>
2022-10-30 13:43:07 +0900

    1030 minjee

--- 백준/투포인터/21921.py
+++ 백준/투포인터/21921.py
@@ -2,13 +2,20 @@
 
 n, x = map(int, input().split())
 data = list(map(int, input().split()))
-
-
-
-
-total = 0
-
-if total == 0:
+max_value = 0
+count = 1 # 기간
+if max(data) == 0:
     print("SAD")
 else:
-    print(total)
+    max_value = sum(data[:x]) # 미리 계산을 저장해둔다.
+    value = max_value
+    for i in range(x, n):
+        value -= data[i-x]
+        value += data[i]
+        if value > max_value:
+            max_value = value
+            count = 1
+        elif value == max_value:
+            count += 1
+    print(max_value)
+    print(count)
